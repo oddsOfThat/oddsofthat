00b9185b8a4173fe1bf48283ac5f43342a8d3e34
oddsOfThat <aidan.a.odonoghue@gmail.com>
2019-07-16 14:24:40 +0100

    Server-side working

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
-const countryData =[];
+const datastore = require("nedb");
+const database = new datastore('database.db');
+database.loadDatabase();
 
+const port = 3000;
+
+const countryData = [];
+var fs = require('fs');
 
 //app.get('/',(req,res) => res.send("Hello World! I'm a response"));
 
@@ -11,9 +16,47 @@ app.listen(port, () => console.log(`Listening on port: ${port}`));
 app.use(express.static('public'));
 app.use(express.json());
 
-app.post("/api", (req, res)=>{
-//console.log(req.body);
-countryData.push(req.body);
-//console.log(countryData);
-res.send("ok!");
+
+
+
+
+app.post("/api", (req, res) => {
+    data= req.body;
+    //Array saving cureent session, not needed
+    countryData.push(data);
+
+    //File storing all country codes and names ever, not needed
+    fs.appendFile('Countrycodes.csv', `${data.countryCode}, ${data.countryName}\n`, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+    // Main API and DB part
+
+
+    database.findOne({
+        "countryCode": data.countryCode
+    }, (err, dbdata) => {
+        if (dbdata == null) {
+            database.insert(data);
+            res.send("Nobody has commented yet!");
+        } else {
+            res.send(dbdata.comment);
+        }
+    });
+
+});
+
+
+
+app.post("/comment", (req, res) => {
+    data = req.body;
+    database.update({
+        "countryCode": data.countryCode
+    }, {
+        $set: {
+            "comment": data.comment
+        }
+    });
+    database.persistence.compactDatafile()
+    res.send("ok!");
 });
--- public/game.js
+++ public/game.js
@@ -1,42 +1,45 @@
 const url = "https://api.wheretheiss.at/v1/coordinates/";
-const url2 = "https://restcountries.eu/rest/v2/alpha/"; 
+const url2 = "https://restcountries.eu/rest/v2/alpha/";
 
 let isRunning = false;
+let isRandom = false;
+let countryCode = "??";
 
 async function LocateMe() {
   if ("geolocation" in navigator) {
-    
-  if (isRunning === false) {
-    isRunning = true;
-    await navigator.geolocation.getCurrentPosition( res =>{ 
-    document.getElementById("latBox").value = res.coords.latitude.toFixed(2);
-    document.getElementById("longBox").value = res.coords.longitude.toFixed(2);
-    submitted();
-    });
-  } else {
-    console.log("The function was already running!");
-  }
+
+    if (isRunning === false) {
+      isRunning = true;
+      await navigator.geolocation.getCurrentPosition(res => {
+        document.getElementById("latBox").value = res.coords.latitude.toFixed(2);
+        document.getElementById("longBox").value = res.coords.longitude.toFixed(2);
+        submitted();
+      });
+    } else {
+      console.log("The function was already running!");
+    }
   } else {
     /* geolocation IS NOT available */
     console.log("Geolocation not available in your browser!");
   }
-  
+
 }
 
 async function submitted() {
   let Lat = 0;
   let Long = 0;
+  countryCode = "??";
   document.getElementById("clue1").innerText = "The Capital is ";
   document.getElementById("clue2").innerText = "The region is ";
   document.getElementById("clue3").innerText = "The flag is ";
   document.getElementById("clue4").src = "https://res.cloudinary.com/dfxgrxdeo/image/upload/v1562262976/white_gqqzxz.bmp";
   document.getElementById("answer").innerText = " ";
+  document.getElementById("comment").innerText = " ";
+  document.getElementById("countryComment").value="";
   getLatLong();
   await getData();
+  isRandom = false;
   isRunning = false;
-
-
-
 }
 
 async function getData() {
@@ -55,24 +58,40 @@ async function getData() {
     document.getElementById("clue2").innerText = "The region is " + data2.region;
     document.getElementById("clue4").src = data2.flag;
     document.getElementById("answer").innerText = "The country is " + data2.name;
-   
-    const apiPost = {'countryCode': data.country_code,'countryName' : data2.name};
-    
-    const options ={
-      method:'POST',
+    countryCode = data.country_code;
+
+    const apiPost = {
+      'countryCode': data.country_code,
+      'countryName': data2.name
+    };
+
+    const options = {
+      method: 'POST',
       headers: {
-        'content-type':'application/json'
+        'content-type': 'application/json'
       },
       body: JSON.stringify(apiPost)
     };
 
-    fetch('/api', options).then( async res => console.log(await res.text()));
-  } else {
+    fetch('/api', options).then(async res => document.getElementById("comment").innerText = await res.text());
+    
+
+
+  } else if(isRandom) {
     document.getElementById("clue1").innerText = "That isn't on land ";
-    document.getElementById("clue2").innerText = " Trying again with random";
+    document.getElementById("clue2").innerText = "We'll try again automatically";
     document.getElementById("clue3").innerText = " ";
+    document.getElementById("clue4").src ="https://res.cloudinary.com/dfxgrxdeo/image/upload/v1563282682/ocean_pvb8zu.jpg";
     isRunning = false;
-    setTimeout(randomm, 1500);
+    isRandom = false;
+    setTimeout(randomm, 2500);
+  }
+  else {
+    document.getElementById("clue1").innerText = "That isn't on land ";
+    document.getElementById("clue2").innerText = "Pick again or try Random";
+    document.getElementById("clue3").innerText = " ";
+    document.getElementById("clue4").src ="https://res.cloudinary.com/dfxgrxdeo/image/upload/v1563282682/ocean_pvb8zu.jpg";
+    isRunning = false;
   }
 }
 
@@ -85,10 +104,32 @@ function getLatLong() {
 function randomm() {
   if (isRunning === false) {
     isRunning = true;
+    isRandom = true;
     document.getElementById("latBox").value = ((Math.random() * 60) - 30).toFixed(2);
     document.getElementById("longBox").value = ((Math.random() * 300) - 150).toFixed(2);
     submitted();
   } else {
     console.log("The function was already running!")
   }
+}
+
+function leaveComment() {
+  
+  const data = {
+    "countryCode": countryCode,
+    "comment": document.getElementById("countryComment").value
+  };
+  console.log(countryCode);
+  if(countryCode!="??"){
+  const options = {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  };
+
+  fetch('/comment', options);
+}
+  document.getElementById("comment").innerText = data.comment;
 }
