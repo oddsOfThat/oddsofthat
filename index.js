8df1500649f81ae63fe75dfc154412becb264b76
oddsOfThat <aidan.a.odonoghue@gmail.com>
2019-10-22 19:51:04 +0100

    ok

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.listen(port, () => console.log(`Listening on port: ${port}`));
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/', (req, res) => res.render('public/index'))
+app.get('/', (req, res) => res.render('public/index'));
+
 app.post("/api", (req, res) => {
     data= req.body;
     //Array saving cureent session, not needed
